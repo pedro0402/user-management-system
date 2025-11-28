99557359ee58a0a4e7c705bead6e63a11f78245c
Pedro Moraes <pedro693.ph@gmail.com>
2025-11-28 16:58:33 -0300

    feat: rota de criacao de usuario com validacao do zod

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,35 @@
 import express from 'express'
 
+import { z } from 'zod'
+import { PrismaClient } from "@prisma/client"
+
 const app = express();
 const PORT = 3333;
+const prisma = new PrismaClient();
+
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     return res.json({message: 'hello world. servidor rodando'})
 })
+
+app.post('/users', async (req, res) => {
+    try {
+        const userSchema = z.object({
+            name: z.string(),
+            email: z.email(),
+            password: z.string()
+        })
+
+        const userData = userSchema.parse(req.body)
+
+        const newUser = await prisma.user.create({ data: userData })
+        
+        return res.status(201).json(newUser)
+    } catch(err) {
+        return res.status(401).json({ error: err })
+    }
+})  
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
