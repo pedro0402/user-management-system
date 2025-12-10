4a7cc747049ab774bc94f73885bb7736c5a6ad1d
Pedro Moraes <pedro693.ph@gmail.com>
2025-12-10 11:15:03 -0300

    refactor: refatorando o schemas para adiciona-los em um novo arquivo na pasta /schemas

---
+++ server/src/schemas/user.schema.ts
@@ -0,0 +1,48 @@
+import { z } from 'zod'
+import { utf8Length } from '../utils/utf8Length';
+
+export const getQuerySchema = z.object({
+    page: z.coerce.number()
+        .min(1)
+        .default(1),
+
+    perPage: z.coerce.number()
+        .min(1)
+        .max(100)
+        .default(10),
+
+    orderBy: z.enum(['createdAt', 'name', 'email', 'updatedAt'])
+        .default('createdAt'),
+
+    orderDirection: z.enum(['asc', 'desc'])
+        .default('desc'),
+
+    search: z.string()
+        .trim()
+        .max(100)
+        .transform(value => value === "" ? undefined : value)
+        .optional(),
+
+    role: z.string()
+        .trim()
+        .optional(),
+})
+
+export const getParamSchema = z.object({
+    id: z.uuid()
+});
+
+export const patchUserSchema = z.object({
+    name: z.string().trim().optional(),
+    email: z.email().toLowerCase().trim().optional(),
+    password: z.string().min(8, 'Senha deve ter pelo menos 8 caracteres')
+        .refine((val) => utf8Length(val) <= 72, {
+            message: 'Senha não corresponde com o tamanho permitido'
+        }).optional()
+})
+
+export const postUserSchema = z.strictObject({
+    name: z.string().trim(),
+    email: z.email().toLowerCase().trim(),
+    password: z.string().min(8, 'Senha deve ter pelo menos 8 caracteres').refine((val) => utf8Length(val) <= 72, { message: 'Senha não corresponde com o tamanho permitido' })
+})
