bed5f7a5a4ab374363a374e8088b8d3d17eac1bd
Pedro Moraes <pedro693.ph@gmail.com>
2025-12-11 15:21:25 -0300

    feat: criação do auth.controller responsável pela chamada do postLogin que serve para autenticação

---
+++ server/src/controllers/auth.controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { prisma } from "../prisma/client";
+import { postLoginSchema } from "../schemas/auth.schema";
+import { compare } from "bcryptjs";
+import { UnauthorizedException } from "../exceptions/UnauthorizedException";
+import { signJwt } from "../utils/jwt";
+
+export const postLogin = async (req: Request, res: Response) => {
+    const { email, password } = postLoginSchema.parse(req.body);
+
+    const user = await prisma.user.findUnique({
+        where: { email }
+    })
+
+    if (!user) {
+        throw new UnauthorizedException('Credenciais inválidas')
+    }
+
+    const comparedPassword = await compare(password, user.password);
+
+    if (!comparedPassword) {
+        throw new UnauthorizedException('Credenciais inválidas')
+    }
+
+    const token = signJwt({
+        id: user.id,
+        email: user.email,
+        role: user.role
+    })
+
+    const { password: _, ...userPublic } = user;
+
+    return res.json({ user: userPublic, token })
+}
