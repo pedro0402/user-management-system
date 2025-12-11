c65f6230fc5a3181f4f445b13640e37cef2cf41c
Pedro Moraes <pedro693.ph@gmail.com>
2025-12-11 16:27:57 -0300

    feat: adiciona função responsável por decodificar e verificar o token jwt

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -2,8 +2,13 @@ import jwt from 'jsonwebtoken';
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-export function signJwt(payload:object): string {
+export function signJwt(payload: object): string {
     return jwt.sign(payload, JWT_SECRET as string, {
         expiresIn: '1h'
     });
+}
+
+export function verifyJwt(token: string): any {
+    if (!JWT_SECRET) throw new Error("JWT não configurado.")
+    return jwt.verify(token, JWT_SECRET);
 }
