4691dbd8e5c0f34ca9de2f2b5d8a8c0574f4754b
Pedro Moraes <pedro693.ph@gmail.com>
2025-12-11 16:29:03 -0300

    feat: adiciona o authMiddleware ao meu user.route(patch, delete) para que apenas usuários válidados possam fazer as operações

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getUsers, getUserById, createUser, updateUser, deleteUser } from "../controllers/user.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 export const userRouter = Router();
 
 userRouter.get('/', getUsers);
 userRouter.get('/:id', getUserById)
 userRouter.post('/', createUser)
-userRouter.patch('/:id', updateUser)
-userRouter.delete('/:id', deleteUser)
+userRouter.patch('/:id', authMiddleware, updateUser)
+userRouter.delete('/:id', authMiddleware, deleteUser)
