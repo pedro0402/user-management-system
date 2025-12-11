f9050c0974284351451b84deae46e382aa295030
Pedro Moraes <pedro693.ph@gmail.com>
2025-12-11 13:39:56 -0300

    feat: adiciona a funcionalidade do middleware ao app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { userRouter } from "./routes/user.route";
+import { errorMiddleware } from "./middlewares/error.middleware";
 
 const app = express();
 
 app.use(express.json());
-app.use('/user', userRouter)
+app.use('/users', userRouter)
+app.use(errorMiddleware);
 
 export { app }
