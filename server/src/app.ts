8f86ae498c26c9fee0149f02f4fd4ae951446d46
Pedro Moraes <pedro693.ph@gmail.com>
2025-12-11 15:19:31 -0300

    feat: adiciona o authRouter ao meu app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { userRouter } from "./routes/user.route";
 import { errorMiddleware } from "./middlewares/error.middleware";
+import { authRouter } from "./routes/auth.route";
 
 const app = express();
 
 app.use(express.json());
-app.use('/users', userRouter)
+app.use('/users', userRouter);
+app.use('/auth', authRouter);
 app.use(errorMiddleware);
 
 export { app }
