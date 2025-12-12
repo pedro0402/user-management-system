f73d1bac51102d958218af18f22ceb5e116a6a2f
Pedro Moraes <pedro693.ph@gmail.com>
2025-12-12 11:14:05 -0300

    feat: adicionando o requireSelfOrRole ao meu user.route

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { getUsers, getUserById, createUser, updateUser, deleteUser } from "../controllers/user.controller";
 import { authMiddleware } from "../middlewares/auth.middleware";
+import { requireSelfOrRole } from "../middlewares/role.middleware";
 
 export const userRouter = Router();
 
-userRouter.get('/', getUsers);
+userRouter.get('/',authMiddleware, requireSelfOrRole("admin"), getUsers);
 userRouter.get('/:id', getUserById)
 userRouter.post('/', createUser)
-userRouter.patch('/:id', authMiddleware, updateUser)
-userRouter.delete('/:id', authMiddleware, deleteUser)
+userRouter.patch('/:id', authMiddleware, requireSelfOrRole("admin"), updateUser)
+userRouter.delete('/:id', authMiddleware, requireSelfOrRole("admin"), deleteUser)
