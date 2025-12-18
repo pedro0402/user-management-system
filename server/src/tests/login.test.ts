1502b33ab76cbd7c2e5811464795f8017ba0d86d
Pedro Moraes <pedro693.ph@gmail.com>
2025-12-18 09:44:00 -0300

    test: adiciona testes para validaão de operação de usuarios com role user e admin

--- server/src/tests/login.test.ts
+++ server/src/tests/login.test.ts
@@ -1,6 +1,5 @@
 import request from 'supertest'
 import { app } from '../app'
-import { email } from 'zod';
 
 describe("autenticacao em rotas protegidas", () => {
     let token: string = "";
@@ -93,5 +92,80 @@ describe('Permissão de acesso ao GET /users', () => {
         const res = await request(app)
             .get('/users');
         expect(res.status).toBe(401);
+    })
+})
+
+describe('Usuário só pode editar/deletar a si mesmo', () => {
+    let userToken: string = '', adminToken: string = '', userId: string = '', otherUserId: string = '';
+
+    beforeAll(async () => {
+        const loginUser = await request(app)
+            .post('/auth/login')
+            .send({
+                email: 'pedro@teste.com',
+                password: 'ph040204'
+            })
+        userToken = loginUser.body.token;
+        userId = loginUser.body.user.id;
+
+        const loginOther = await request(app)
+            .post('/auth/login')
+            .send({
+                email: 'carlos@teste.com',
+                password: '12345678'
+            })
+        otherUserId = loginOther.body.user.id;
+
+        const loginAdmin = await request(app)
+            .post('/auth/login')
+            .send({
+                email: 'adminteste@teste.com',
+                password: '12345678'
+            })
+        adminToken = loginAdmin.body.token;
+    });
+
+    it('user comum pode editar/deletar SÓ ele mesmo (PATCH)', async () => {
+        const res = await request(app)
+            .patch(`/users/${userId}`)
+            .set('Authorization', 'Bearer ' + userToken)
+            .send({ name: 'pedro moraes' })
+        expect(res.status).toBe(200);
+        expect(res.body.name).toBe('pedro moraes')
+    });
+
+    it('user comum NÃO pode editar outros usuários', async () => {
+        const res = await request(app)
+            .patch(`/users/${otherUserId}`)
+            .set('Authorization', 'Bearer ' + userToken)
+            .send({ name: 'carlos magno' })
+        expect(res.status).toBe(403);
+    });
+
+    it('user comum pode deletar SÓ ele mesmo', async () => {
+        const res = await request(app)
+            .delete(`/users/${userId}`)
+            .set('Authorization', 'Bearer ' + userToken);
+        expect(res.status).toBe(204);
+    })
+
+    it('user comum NÃO pode deletar outros usuários', async () => {
+        const res = await request(app)
+            .delete(`/users/${otherUserId}`)
+            .set('Authorization', 'Bearer ' + userToken);
+        expect(res.status).toBe(403);
+    })
+
+    it('admin pode editar/deletar qualquer usuário', async () => {
+        const resEdit = await request(app)
+            .patch(`/users/${otherUserId}`)
+            .set('Authorization', 'Bearer ' + adminToken)
+            .send({name: 'pedro souza moraes'});
+        expect(resEdit.status).toBe(200);
+
+        const resDelete = await request(app)
+            .delete(`/users/${otherUserId}`)
+            .set('Authorization', 'Bearer ' + adminToken)
+        expect([200, 204]).toContain(resDelete.status);
     })
 })
