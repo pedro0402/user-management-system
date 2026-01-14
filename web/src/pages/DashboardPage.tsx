0389e75a2a850494643be93149dab7f2a743481f
Pedro Moraes <pedro693.ph@gmail.com>
2026-01-14 16:37:32 -0300

    feta: funcionalidade para mostrar a role do usuario no dashboard

--- web/src/pages/DashboardPage.tsx
+++ web/src/pages/DashboardPage.tsx
@@ -1,11 +1,13 @@
 export function DashboardPage() {
 
     const userEmail = localStorage.getItem("email");
+    const userRole = localStorage.getItem("role");
 
     return (
         <div>
             <h1>Bem vindo ao Dashboard!</h1>
             <p>voce esta logado como: {`${userEmail}`}</p>
+            <p>role do usuário: {`${userRole}`}</p>
         </div>
     )
 
