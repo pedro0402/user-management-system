67c7b70f79efb84b71f3b5a60790ce0d3b9d1263
Pedro Moraes <pedro693.ph@gmail.com>
2026-01-14 16:09:24 -0300

    refactor: ajustando o componente PrivateRoute

--- web/src/components/PrivateRoute.tsx
+++ web/src/components/PrivateRoute.tsx
@@ -4,11 +4,11 @@ import type React from "react";
 function PrivateRoute({ children }: { children: React.ReactNode }) {
     const token = localStorage.getItem("token");
 
-    if (token === null) {
+    if (!token) {
         return <Navigate to='/login' replace />
     }
 
-    return token ? children : <Navigate to="/login" />
+    return <>{children}</>
 }
 
 export default PrivateRoute;
