769492cf33d792b38f6a8dd36f1c577c8adcf683
Pedro Moraes <pedro693.ph@gmail.com>
2026-01-14 16:11:51 -0300

    feat: criacao da pagina de Dashboard

---
+++ web/src/pages/DashboardPage.tsx
@@ -0,0 +1,11 @@
+export function DashboardPage() {
+
+    const userEmail = localStorage.getItem("email");
+
+    return (
+        <div>
+            <h1>Bem vindo ao Dashboard!</h1>
+        </div>
+    )
+
+}
