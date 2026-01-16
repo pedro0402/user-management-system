8cfe4f040ab084d73e6bd2370c71c3c1c50e45e4
Pedro Moraes <pedro693.ph@gmail.com>
2026-01-16 15:52:23 -0300

    feat: criacao de classe com metodos utilitarios

---
+++ web/src/services/utils.ts
@@ -0,0 +1,10 @@
+export function sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export function validateName(value: string) {
+    if (!value.trim()) return "O nome é obrigatório";
+    if (value.trim().length < 3) return "O nome deve ter no minímo 3 caracteres";
+    if (value.trim().length > 30) return "O nome deve ter no máximo 30 caracteres";
+    return null;
+}
